95b0c239c946c1c558e381baa70e256ef43c4113
Jac <lu7766lu7766@gmail.com>
2019-07-17 12:05:14 +0800

    套件整理

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import _ from 'lodash'
+import moment from 'moment'
+import { JacPlugin } from '../../JacTools/index'
 import MyPlugin from 'src/plugin'
 
+Vue.use(JacPlugin, {
+  _, moment
+})
 Vue.use(MyPlugin)
 
 Vue.config.productionTip = false
